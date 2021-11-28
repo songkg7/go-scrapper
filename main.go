334b78ce8b3b3ce8aaa10daf5c37ff10184b3800
haril <songkg7@gmail.com>
2021-11-28 12:03:07 +0900

    Functions 2

--- main.go
+++ main.go
@@ -6,22 +6,13 @@ import (
 )
 
 func main() {
-	fmt.Println(multiply(2, 2))
-	// multi value 는 반드시 값을 받아야하기 때문에 무시하기 위해서는 underscore 를 사용한다.
-	totalLength, _ := lenAndUpper("haril")
-	fmt.Println(totalLength)
-
-	repeatMe("scala", "java", "python")
+	totalLength, upper := lenAndUpper("haril")
+	fmt.Println(totalLength, upper)
 }
 
-func multiply(a, b int) int {
-	return a * b
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
