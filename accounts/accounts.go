4f66a57555b59bb1a1e76156a4fd2934e54acfe6
haril <songkg7@gmail.com>
2021-11-28 14:14:13 +0900

    Method 2

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,11 +1,16 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
 
+var errNoMoney = errors.New("can't withdraw")
+
+// Create account
 func Create(owner string) *Account {
 	account := Account{
 		owner:   owner,
@@ -15,11 +20,28 @@ func Create(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+// NOTE: Go 의 method 는 receiver 를 복사하여 사용한다. 이로 인해 모든 객체의 불변성을 보장할 수 있다.
+func (a Account) Deposit(amount int) *Account {
 	a.balance += amount
+	return &a
+}
+
+// Owner of your account
+func (a Account) Owner() string {
+	return a.owner
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+// NOTE: if you want mutable object, you can use pointer "*"
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"scrapper/accounts"
 )
 
 func main() {
-	account := accounts.Create("haril")
-	account.Deposit(10)
+	account := accounts.Create("haril").Deposit(10)
+	fmt.Println(account.Owner())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(account.Balance())
 }
