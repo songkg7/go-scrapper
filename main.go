2be22d44d28c6c61bb18389fe1829b4b675470c3
haril <songkg7@gmail.com>
2021-11-28 12:20:50 +0900

    Switch

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ func main() {
 }
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
 }
