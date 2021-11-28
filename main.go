539439efd2dadfa4ae9e6574f864bbfdaabcdc7b
haril <songkg7@gmail.com>
2021-11-28 12:40:27 +0900

    Maps

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// Go 의 slice 는 immutable 하다.
-	names := []string{"java", "python", "go"}
-	newNames := append(names, "scala")
-	fmt.Println(newNames)
+	note := map[string]string{"name": "haril", "age": "28"}
+	fmt.Println(note)
 }
