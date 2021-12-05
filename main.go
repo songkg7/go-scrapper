adb87441a7c7302e65db123bac352f7bdc22e267
haril <songkg7@gmail.com>
2021-12-05 23:21:49 +0900

    [refactor]

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
+	var results = make(map[string]string)
+
 	urls := []string{
 		"https://www.airbnb.com/",
-		"https://www.google.com/",
 		"https://www.amazon.com/",
 		"https://www.reddit.com/",
 		"https://www.google.com/",
@@ -18,19 +19,25 @@ func main() {
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
 	}
-
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
 	}
-
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
 }
 
 var errRequestFailed = errors.New("request failed")
 
 func hitURL(url string) error {
-	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
