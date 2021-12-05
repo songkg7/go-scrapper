cb298bb7b89ab8d575a4f86987473b9b63859cfb
haril <songkg7@gmail.com>
2021-12-05 23:42:35 +0900

    go routines 기본 사용법

--- main.go
+++ main.go
@@ -1,44 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
 func main() {
-	var results = make(map[string]string)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+	channel := make(chan bool)
+	people := [2]string{"haril", "alex"}
+	for _, person := range people {
+		go isSexy(person, channel)
 	}
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	fmt.Println(<-channel)
+	fmt.Println(<-channel)
 }
 
-var errRequestFailed = errors.New("request failed")
-
-func hitURL(url string) error {
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
+func isSexy(person string, channel chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	channel <- true
 }
