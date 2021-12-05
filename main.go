ad0e88149a9c8b28b0f4168ff3c463f26cc07d6f
haril <songkg7@gmail.com>
2021-12-05 23:09:57 +0900

    hitURL

--- main.go
+++ main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"scrapper/dict"
+	"net/http"
 )
 
 func main() {
-	dictionary := dict.Dictionary{"first": "first word"}
-	baseWord := "hello"
-	errAdd := dictionary.Add(baseWord, "First")
-	if errAdd != nil {
-		fmt.Println(errAdd)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
 
-	err := dictionary.Update(baseWord, "Second")
-	if err != nil {
-		fmt.Println(err)
+	for _, url := range urls {
+		hitURL(url)
 	}
-	word, _ := dictionary.Search(baseWord)
-	fmt.Println(word)
+
+}
+
+var errRequestFailed = errors.New("request failed")
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	return nil
 }
