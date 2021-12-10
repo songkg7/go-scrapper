eb28bd992594033b6d85147322e4eae6cb9750d6
haril <songkg7@gmail.com>
2021-12-10 20:32:58 +0900

    Channels Recap

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-	channel := make(chan bool)
+	channel := make(chan string)
 	people := [2]string{"haril", "alex"}
 	for _, person := range people {
 		go isSexy(person, channel)
 	}
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-channel)
+	}
 }
 
-func isSexy(person string, channel chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	channel <- true
+func isSexy(person string, channel chan string) {
+	time.Sleep(time.Second * 10)
+	channel <- person + " is sexy"
 }
